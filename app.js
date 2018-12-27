6241bbb8b396b4d9f44da6656ff302ecb9073774
chenmeng <chenmeng@baizitech.com>
2018-12-27 14:55:09 +0800

    b

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ const jwt = require('jsonwebtoken')
 const expressJwt = require('express-jwt')
 const cors = require('cors')
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.static(__dirname))
 
+//---data
 const users = [
     { id: 1, username: 'admin', password: 'admin' },
     { id: 2, username: 'guest', password: "guest" }
 ]
 
 const secret = 'my secret key'
+//--
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(__dirname))
+app.use(expressJwt({ secret }).unless({ path: ['/login', '/resource'] }))
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401)
+            .send('invalid token')
+    }
+})
 
 app.post('/login', (req, res) => {
     if (!req.body.username || !req.body.password) {
@@ -42,7 +52,8 @@ app.post('/login', (req, res) => {
 app.get('/resource', (req, res) => {
     res.end('public')
 })
-app.get('/resource/secret', expressJwt({ secret }), (req, res) => {
+app.get('/resource/secret', (req, res) => {
+    console.log(req.user)
     res.end('secret')
 })
 
